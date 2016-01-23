2c5e71781d90ea96493ca55259a93a2b6a3af0ad
zvolsgra <zvolsgra@fit.cvut.cz>
2016-01-23 15:52:28 +0000

    Remove REST API. WebSocket will be used instead.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,32 +1,17 @@
 package controllers
 
-import javax.inject.{Singleton, Inject}
+import javax.inject.{Inject, Singleton}
 
 import actors.ClientConnection.ClientEvent
-import actors.{DockerClientSupervisor, ClientConnection}
+import actors.{ClientConnection, DockerClientSupervisor}
 import akka.actor._
-import com.github.dockerjava.api.model.Container
-import com.github.dockerjava.core.DockerClientConfig
-import play.api.libs.json.JsValue
-import play.api.mvc._
 import play.api.Play.current
-import services.DockerService
-
-import scala.concurrent.Future
-import scala.collection.mutable.Buffer
-import scala.concurrent.ExecutionContext.Implicits.global // TODO remove and move API to a separate controller
+import play.api.mvc._
 
 @Singleton
 class Application @Inject() (system: ActorSystem) extends Controller {
   val dockerClientSupervisorProps = Props[DockerClientSupervisor]
   val topLevelActor = system.actorOf(dockerClientSupervisorProps)
-
-  val config = DockerClientConfig.createDefaultConfigBuilder()
-    .withVersion("1.17")
-    .withUri("http://127.0.0.1:4243")
-    .build()
-
-  val docker = new DockerService(config)
 
   def index = Action { implicit req =>
     Ok(views.html.index("Your new application is ready."))
@@ -34,10 +19,6 @@ class Application @Inject() (system: ActorSystem) extends Controller {
 
   def containers = Action { implicit req =>
     Ok(views.html.containers())
-  }
-
-  def getContainers = Action.async { implicit request =>
-    docker.getContainers.map { containers => Ok(containers unapply)}
   }
 
   def stream(email: String) = WebSocket.acceptWithActor[ClientEvent, ClientEvent] { _ => upstream =>
